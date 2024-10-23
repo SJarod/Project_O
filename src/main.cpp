23e0e5dd447fe4ce990f0fb15251c8594ea19a20
SJarod <91637364+SJarod@users.noreply.github.com>
2024-10-23 20:48:00 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
+# TODO : render in fbo instead of swapchain image directly (google search "bgra vs rgba")
+# TODO : clean la compilation et l’installation (glslc, install)
+
+
 #include <iostream>
 
 #include "application.hpp"
